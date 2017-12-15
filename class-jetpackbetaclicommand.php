331fe50167d18cdf3f9168c0be5cee049c4dedfa
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2017-12-15 11:34:34 +0700

    Add CliCommand PR installs

--- class-jetpackbetaclicommand.php
+++ class-jetpackbetaclicommand.php
@@ -10,15 +10,15 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		 *
 		 * ## OPTIONS
 		 *
-		 *
 		 * activate master: Get a version of the master branch built every 15 minutes
-		 *
 		 * activate stable: Get the latest stable version of Jetpack
-		 *
+		 * activate pr branch_name: Get a version of PR. PR must be built and unit-tested before it become availabe
+		 * 
 		 * ## EXAMPLES
 		 *
 		 * wp jetpack-beta branch activate master
 		 * wp jetpack-beta branch activate stable
+		 * wp jetpack-beta branch activate pr branch_name
 		 *
 		 */
 		public function branch( $args ) {
@@ -39,7 +39,17 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 				}
 				return WP_CLI::success( __( 'Jetpack is currently on Latest Stable', 'jetpack-beta' ) );
 			}
-			return WP_CLI::error( __( 'Unrecognized version', 'jetpack' ) );
+
+			if ( 'pr' === $args[1] && ! empty( $args[2] ) ) {
+				$branch_name = $args[2];
+				$branch_name = str_replace('/', '_', $branch_name);  
+				$retvalue = Jetpack_Beta::install_and_activate( $branch_name, 'pr' );
+				if ( is_wp_error( $retvalue ) ) {
+					return WP_CLI::error( __( 'Error', 'jetpack' ) . $retvalue->get_error_message() );
+				}
+				return WP_CLI::success( __( 'Jetpack is currently on ' . $branch_name, 'jetpack-beta' ) );
+			}
+			return WP_CLI::error( __( 'Unrecognized section or PR', 'jetpack' ) );
 		}
 
 		private function validation_checks($args) {
