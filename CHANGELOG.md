27933161403a9f7796efe2c7e2afed9dd5786468
kraftbj <kraftbj@users.noreply.github.com>
2021-07-30 23:24:34 +0000

    Tools: Update eslint-plugin-jsdoc (#20515)
    * JSDoc updates
    
    * Bump to 35.x
    
    * Bump to 36.x
    
    * Bump beta plugin
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1083845381

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 3.0.3-alpha - unreleased
+
+This is an alpha version! The changes listed here are not final.
+
 ## 3.0.2 - 2021-07-29
 ### Added
 - Use WP core's ajax updater to apply updates.
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_2
+class ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_3_alpha
 {
     public static $prefixLengthsPsr4 = array (
         'C' => 
@@ -71,10 +71,10 @@ class ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_2
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_2::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_2::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_2::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_2::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_3_alpha::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_3_alpha::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_3_alpha::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit567fa3f555de8fd218dfdc1688bb97b5_betaⓥ3_0_3_alpha::$classMap;
 
         }, null, ClassLoader::class);
     }
