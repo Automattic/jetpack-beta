3f7abddd3e011799faa1eff40170497d43a7e4e0
sdixon194 <sdixon194@users.noreply.github.com>
2021-11-23 18:50:33 +0000

    Widget Visibility: Apply widget filtering to customizer preview (#21505)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1496174450

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '09ae39c1a7cbc051b4791e26135a0907afbb4078',
+            'reference' => 'a4d4e94448b3cff7d928548ad1c340178f5be20d',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
