e48fc23d5d8630877e03e139cc2463f71f4c61fd
kangzj <kangzj@users.noreply.github.com>
2021-11-24 20:24:52 +0000

    Search: Add essential scaffolding for package (#21814)
    * [not verified] Add eslint config
    
    * [not verified] Add babel config
    
    * [not verified] Add postcss config
    
    * [not verified] Add jest config
    
    * [not verified] Add package.json
    
    * [not verified] Bump composer version
    
    Co-authored-by: Jasper Kang <jasper.kang@automattic.com>
    Co-authored-by: Jasper Kang <kangzjnet@gmail.com>
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1501008416

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'a4d4e94448b3cff7d928548ad1c340178f5be20d',
+            'reference' => 'b4137a70a9fe20ebb40554f7025cc16716986671',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
