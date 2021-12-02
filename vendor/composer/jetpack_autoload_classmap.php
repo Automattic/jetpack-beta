f1112fcad6ba151254bba1f332756ba2b539d667
ivan-ottinger <ivan-ottinger@users.noreply.github.com>
2021-12-02 12:46:18 +0000

    Twitter Timeline widget: widget deprecation
    This commit hides the Twitter Timeline widget from the block inserter and Legacy widget block drop-down menu.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1530573633

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -6,37 +6,9 @@ $vendorDir = dirname(__DIR__);
 $baseDir   = dirname($vendorDir);
 
 return array(
-	'Composer\\Semver\\Semver' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
-	),
-	'Composer\\Semver\\CompilingMatcher' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
-	),
-	'Composer\\Semver\\Intervals' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
-	),
 	'Composer\\Semver\\VersionParser' => array(
 		'version' => '3.2.6.0',
 		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
-	),
-	'Composer\\Semver\\Interval' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
-	),
-	'Composer\\Semver\\Comparator' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
-	),
-	'Composer\\Semver\\Constraint\\MatchAllConstraint' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
-	),
-	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
-		'version' => '3.2.6.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
 	),
 	'Composer\\Semver\\Constraint\\Constraint' => array(
 		'version' => '3.2.6.0',
@@ -46,6 +18,14 @@ return array(
 		'version' => '3.2.6.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchNoneConstraint.php'
 	),
+	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
+	),
+	'Composer\\Semver\\Constraint\\MatchAllConstraint' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
+	),
 	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
 		'version' => '3.2.6.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
@@ -54,9 +34,25 @@ return array(
 		'version' => '3.2.6.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/Bound.php'
 	),
-	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
-		'version' => '2.10.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadFileWriter.php'
+	'Composer\\Semver\\Intervals' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
+	),
+	'Composer\\Semver\\Interval' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
+	),
+	'Composer\\Semver\\CompilingMatcher' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
+	),
+	'Composer\\Semver\\Comparator' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
+	),
+	'Composer\\Semver\\Semver' => array(
+		'version' => '3.2.6.0',
+		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadGenerator' => array(
 		'version' => '2.10.9999999.9999999-dev',
@@ -66,29 +62,25 @@ return array(
 		'version' => '2.10.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
 	),
+	'Automattic\\Jetpack\\Autoloader\\AutoloadProcessor' => array(
+		'version' => '2.10.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
+	),
 	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
 		'version' => '2.10.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
 	),
-	'Automattic\\Jetpack\\Autoloader\\AutoloadProcessor' => array(
+	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
 		'version' => '2.10.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadFileWriter.php'
 	),
 	'Parsedown' => array(
 		'version' => '1.7.4.0',
 		'path'    => $vendorDir . '/erusev/parsedown/Parsedown.php'
 	),
-	'Automattic\\JetpackBeta\\Hooks' => array(
+	'Automattic\\JetpackBeta\\Utils' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-hooks.php'
-	),
-	'Automattic\\JetpackBeta\\Plugin' => array(
-		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-plugin.php'
-	),
-	'Automattic\\JetpackBeta\\CliCommand' => array(
-		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-clicommand.php'
+		'path'    => $baseDir . '/src/class-utils.php'
 	),
 	'Automattic\\JetpackBeta\\Admin' => array(
 		'version' => 'dev-master',
@@ -98,16 +90,24 @@ return array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-plugindataexception.php'
 	),
-	'Automattic\\JetpackBeta\\Utils' => array(
+	'Automattic\\JetpackBeta\\Hooks' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-utils.php'
+		'path'    => $baseDir . '/src/class-hooks.php'
+	),
+	'Automattic\\JetpackBeta\\CliCommand' => array(
+		'version' => 'dev-master',
+		'path'    => $baseDir . '/src/class-clicommand.php'
+	),
+	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
+		'version' => 'dev-master',
+		'path'    => $baseDir . '/src/class-autoupdateself.php'
 	),
 	'Automattic\\JetpackBeta\\ParsedownExt' => array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-parsedownext.php'
 	),
-	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
+	'Automattic\\JetpackBeta\\Plugin' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-autoupdateself.php'
+		'path'    => $baseDir . '/src/class-plugin.php'
 	),
 );
