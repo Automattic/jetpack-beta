8f16bacdbdba911f92bd8886739d6056c9f4a704
anomiex <anomiex@users.noreply.github.com>
2021-12-22 18:39:05 +0000

    Assign unique text domains to our packages (#22151)
    To finally fix #21690, we need each package to have its own textdomain and
    to declare that domain in its `composer.json`.
    
    The work done in previous PRs will take it from there: composer-plugin will
    include the packages' domains in the generated `i18n-map.php` (#22096),
    which the plugin will pass to the Assets package (#22143), which will map
    those package textdomains to the plugin's (#22083).
    
    This also adds checks to the monorepo linting script to help people get the
    setup right:
    
    * Packages may not use a textdomain that matches a plugin slug.
    * If the package's phpcs config or eslint config sets a textdomain (which it
      will have to if it uses i18n after #22117 and #22131), require that same
      textdomain is set in composer.json.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1612559464

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '3abc28be7bd526fe35c9dec649acf937083cd755',
+            'reference' => '198e6ad84f889b5cc78051095a7896aece64ae05',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
