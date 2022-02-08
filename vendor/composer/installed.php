7fcf89e65d6a55a917d6a949e80ebbe738940efd
kraftbj <kraftbj@users.noreply.github.com>
2022-02-08 19:05:46 +0000

    PHPCS: SSO (#22610)
    * Remove files from exclude list
    
    * Run phpcs:fix auto fixes
    
    * Add changelog
    
    * class.jetpack-sso-notices.php: add file doc comment
    
    * class.jetpack-sso-helpers.php: docblock comments and function visibility
    
    * class.jetpack-sso-helpers.php: fix loose comparisons
    
    * class.jetpack-sso-helpers.php: use wp_rand over mt_rand
    
    * class.jetpack-sso-helpers.php: fix var assignment in conditional
    
    * sso.php: fix docblock comments and function visibility
    
    * sso.php: fix variable assignment issues
    
    * sso.php: fix output escaping issues
    
    * sso.php: use rawurlencode over urlencode
    
    * sso.php: EnqueuedResourceParameters.NotInFooter
    
    * sso.php: add a MissingTranslatorsComment
    
    * sso.php: fix loose comparisons
    
    * sso.php: update an ignored line
    
    * sso.php: ignore some NonceVerification warnings
    
    SSO form is being nonced, then verified in jetpack_xmlrpc_sso_validate_result
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1814131072

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'd715e728658dee65e1697dba70f0d5dcf2180177',
+            'reference' => '273ce43e4574a60705b0ad29374ecad9ad2cfbfa',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
