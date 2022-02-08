ce12ae9b71feaedf83e8968df377c5983caac104
leogermani <leogermani@users.noreply.github.com>
2022-02-08 20:12:55 +0000

    My Jetpack: Remove search dependency (#22732)
    * check for search status
    
    * changelog and lock files
    
    * changelogs
    
    * Update projects/packages/my-jetpack/src/products/class-search.php
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1814426378

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '273ce43e4574a60705b0ad29374ecad9ad2cfbfa',
+            'reference' => 'ddd1e92e829724460b6a8dd08b69edea4a8f7039',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
