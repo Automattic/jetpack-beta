34267d7d83dcc3cc1a0af4345b4e8e795f31c606
retrofox <retrofox@users.noreply.github.com>
2022-02-09 19:58:40 +0000

    My Jetpack: Add 'unlinked' parameter to the checkout URL when the user is not connected (#22740)
    * [not verified] my-jetpack: add unlink to checkout link when no user connected
    
    * [not verified] changelog
    
    * [not verified] my-jetpack: re-implement building the checkout URL
    
    * [not verified] my-jetpack: use encodeURIComponent for the redirectUrl
    
    * my-jetpack: re-factoring the product checkout URL
    
    * my-jetpack: update jsdoc
    
    * my-jeypack: ensure getProductCheckoutUrl() return string
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1820106336

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'f2608b711d90b810552a082d837db866aae72cea',
+            'reference' => '272cf480724bdee7d260988ed1f307e103e8e3f2',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
