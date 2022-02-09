62a0f2cd4735ecd6ea25ca3aae9ba90f5473f3ad
sdixon194 <sdixon194@users.noreply.github.com>
2022-02-09 14:51:42 +0000

    Fix broken image in plan loading placeholder in "My Plan" (#22750)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1818648706

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '2f17c6906fce7324aa173e71e5bbea4b2e0af7e7',
+            'reference' => '9a3a205dce26c61a9bb84981bfb07ffdcfb2a0d3',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
