7a77f1e6c37415f1b27f399c829705e6a72fbebb
anomiex <anomiex@users.noreply.github.com>
2022-02-10 15:31:16 +0000

    pr-is-up-to-date: Update docs (#22793)
    The documentation of the token parameter to the action in README.md was
    not updated after being copy-pasted from push-to-mirrors. Updated it to
    match the documentation in action.yml, which had been updated at some
    point.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1824573318

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '719c5020bf8ecd3dd869f1f0f9b4ab1648f8cd8b',
+            'reference' => 'dc6b56ef677f288265a994d5dbf283e2c17eb751',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
