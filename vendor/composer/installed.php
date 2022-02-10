b01b0557b7d90f814206cce481beff8d0936e1b6
anomiex <anomiex@users.noreply.github.com>
2022-02-10 16:13:04 +0000

    cli: Remove addition of changelogger in `--for-mirrors` (#22765)
    This was originally added (with an incorrect slug) because the mirroring
    invokes changelogger in order to update the changelog.
    
    But since the mirroring also forces building of dependencies, and it
    only runs changelogger if the dependencies call for it, we don't
    actually need to do that since changelogger will already have been
    called for that way.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1824766534

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'dc6b56ef677f288265a994d5dbf283e2c17eb751',
+            'reference' => '43d9116c465cb3d8fad6480fb8e0c8d57c8f7b8a',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
