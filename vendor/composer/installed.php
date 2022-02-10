a676ec1dc6bde48bf1f24bb8da8a81aba0201a31
oskosk <oskosk@users.noreply.github.com>
2022-02-10 00:17:38 +0000

    My Jetpack: Avoid fetch all products on initial render (#22782)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1821107086

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'fff329ffb2c4a4d9f07857e64a8d93e918da422c',
+            'reference' => '26c4036646573992cb14119bc982f7dd6dadafd3',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
