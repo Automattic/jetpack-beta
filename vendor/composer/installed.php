92f6b39ea32cb4b58501e2f8f211347d7e486734
jeherve <jeherve@users.noreply.github.com>
2022-02-11 10:31:47 +0000

    Repeat Visitor block: Add the Align tool (Wide and Full Width) (#22703)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1828760986

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '64c59238c5321d81c2866e1bed74d71f29a51f05',
+            'reference' => '56b174e5e76741009d61adaf47005ce1920692c2',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
