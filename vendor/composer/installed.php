f08a974555ed06b6d589e0f9a31be7f066236bbe
kallehauge <kallehauge@users.noreply.github.com>
2022-02-11 20:31:58 +0000

    Jetpack plugin: Remotely check for partner coupon validity (#22420)
    * Partner coupon: remotely check availability
    
    * Add changelog files
    
    * Use blog token requests
    
    * Update endpoint and expected return
    
    * Fix existing and add new unit tests
    
    * Update Jetpack plugin dependencies after partner package update
    
    * Add Jetpack plugin changelog
    
    * Bail early if we do not have a coupon added date
    
    The added date and the coupon itself are handled at the same time. One should not exist if the other do not.
    
    * Update package dependencies
    
    * Convert "last check" from option to transient
    
    * Revert composer option version
    
    * Remove unused changelog files
    
    * Reverse plugin dependency updates
    
    * Reverse Jetpack plugin composer (reverse options version)
    
    * Add defensive coding to maybe_purge_coupon_by_availability_check
    
    * Use add_query_arg to append query param
    
    * Rename $jetpack_id to $blog_id
    
    * Refactor requets_as_blog callable logic
    
    * Update test after refactoring callable logic
    
    * Update JITM to use latest alpha version
    
    * Use older versio of MockBuilder::addMethods
    
    We do this to support our PHP 5.6 tests as well.
    
    * Address comment/transient name feedback
    
    * Fix Jetpack composer.lock conflict
    
    * Update dependency back
    
    See https://github.com/Automattic/jetpack/pull/22420#discussion_r802905297
    
    * Update lock file
    
    * Bail early if site is not connected
    
    * Bail early if site is in offline mode
    
    * Update Jetpack plugin dependencies
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1831355713

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '32c5d1380a989d5c891291af3d623cc65a966b9c',
+            'reference' => '2f70599f32a552ab3539505517973158b5763172',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
