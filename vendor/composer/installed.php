087a2ab680a4cfc2793e646916774e7bd38f7054
ivan-ottinger <ivan-ottinger@users.noreply.github.com>
2022-02-14 08:51:02 +0000

    Mailchimp block: Add Align, Color and Spacing features (#22622)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1839992740

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '61b72541adf1f96bee6b8e3acf46b2ff78c74fa1',
+            'reference' => '5aace5c3d77baaa872827c279def2732667634c5',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
