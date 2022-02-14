91c8bf014cb23570b829e2e19d977462e15b294a
oskosk <oskosk@users.noreply.github.com>
2022-02-14 19:48:48 +0000

    My Jetpack: Improve global notice style (#22861)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1843108244

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'aad8815bd7779bf92ac8a2e899dbde22f32a5c84',
+            'reference' => '178ad3dab5f5df1f426ce8575d16f9041c28fe46',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
