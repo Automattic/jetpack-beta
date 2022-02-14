0c4d00706ee781a0443b095808ac895b885e1e5a
retrofox <retrofox@users.noreply.github.com>
2022-02-14 19:54:08 +0000

    My Jetpack: add VideoPress product data  (#22862)
    * my-jetpack: add VideoPress data
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1843120691

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '7c03f6154bf24fb0ca32f51ac057fcd4bfe897b7',
+            'reference' => 'a079d1e430f953dcb4d446f694ccb5d07424db4c',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
