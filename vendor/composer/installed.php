5c9979aad29e9f00da84b8331b2ac7d82ab41f17
retrofox <retrofox@users.noreply.github.com>
2022-02-14 21:34:34 +0000

    My Jetpack: Make click on Fix connection show connection route (#22868)
    * Make click on Fix connection show connection screen
    
    * [not verified] Add changelog file
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1843574643

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'fd0d460153ca1c494afdb6f0a27261187711fb5a',
+            'reference' => '1aaf52c1b5a4440e4d04a12f8ba4653a8aa8ad69',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
