c2b15b8cd3646e3a3e53bafcba049f717147cdd4
anomiex <anomiex@users.noreply.github.com>
2022-02-14 08:21:29 +0000

    actions: Send a Slack notification on workflow failure on master (#22821)
    This workflow runs after certain other workflows have completed on
    pushes to master or release branches. If the other workflow failed, it
    will post a message about it to Slack.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1839861193

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'e751b76dd1136c7238471b1ab65091ef69a43589',
+            'reference' => '5c0e4b7dccb40875f5639cd5c1b30c8753010775',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
