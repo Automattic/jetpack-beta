25a8beb1520ed2635c98375e87d276ce84ec58b5
oskosk <oskosk@users.noreply.github.com>
2022-02-16 18:44:14 +0000

    My Jetpack: Fix Add click for CRM, VideoPress and Anti-Spam product cards (#22923)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1854566489

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c581928dc1da3e61018b376d4308b179b385abb3',
+            'reference' => '4ea9a6f0150e1d28955e88ffbd116d82f3362b24',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
