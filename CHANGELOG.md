95f6f73d5cde8b6ce9e50ed86021a08a3db98591
sdixon194 <sdixon194@users.noreply.github.com>
2022-02-16 16:10:13 +0000

    Jetpack 10.7-a.5 release and composer bump (#22920)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1853820432

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ This is an alpha version! The changes listed here are not final.
 ### Added
 - Added docs to JS file.
 
+### Changed
+- Updated composer.lock
+
 ## 3.1.0 - 2021-12-08
 ### Added
 - Added an action to auto-create a GitHub release when a version is tagged.
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '26fd4990239ac7f9700f3e465efe7cc5b61b6742',
+            'reference' => 'f8fc84032d825ffb5a4f0868b875d0b53b3aae6c',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
