c3b5365d031b1038595de30e94b5339b8364b3d4
renatoagds <renatoagds@users.noreply.github.com>
2022-02-16 20:39:14 +0000

    My Jetpack: Fix Add click for Scan and Search product cards (#22929)
    * Make Scan and Search interstitials install the plugin
    
    * Add changelog file
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1855073453

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'cf9979a700d52f2256b6233f2e5359aba80b1c27',
+            'reference' => 'a751044c0bd9ca0cf9b1f159147cb46da95864cb',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
