f6d537d90b10a0fef3a776b895dcd2bdc7c31a44
oskosk <oskosk@users.noreply.github.com>
2022-02-17 12:51:58 +0000

    My Jetpack: Apply coupon discount to Product price when it exists (#22932)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1858678199

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '9fb2ad88e4d8227c0e4dcc500c1b563824f20633',
+            'reference' => 'd34753d2c12abb0cba4a888c04e6340e6a163b6f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
