54ec098cb554297f97250b95c7c69a40a238307f
pablinos <pablinos@users.noreply.github.com>
2022-02-18 11:07:36 +0000

    Publicize: Fix double escaping in Classic Editor (#22974)
    * Fix double escaping in Classic Editor
    
    * Add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1863989162

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'fa9f5ed73fc238c69b11d4b77f98938923589c7f',
+            'reference' => '9c8ce58464a87822ea933b48c67e1b613943e387',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
