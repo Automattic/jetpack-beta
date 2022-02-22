6846a943998bb124f640fdb02a1184ec6f9aceea
kraftbj <kraftbj@users.noreply.github.com>
2022-02-22 20:29:01 +0000

    My Jetpack: Use Jetpack namespace to fix fatal error (#23041)
    * my-jetpack: use Jetpack in Backup class
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1883761593

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '79a1756707a6cbdd248dd2493d8dec67fca4adad',
+            'reference' => 'a910d2c17209dd62885ce73a00eb84b548ecc3b1',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
