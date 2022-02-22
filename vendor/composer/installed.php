7e09eca687c43e83814f273527197ccc35f5e8ba
kraftbj <kraftbj@users.noreply.github.com>
2022-02-22 02:08:41 +0000

    My Jetpack: Remove check for feature flag JETPACK_ENABLE_MY_JETPACK (#23011)
    * Remove conditional loading of My Jetpack
    
    * Add changelog file
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1879211826

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '4e463e1ad99e63227038b654d14fb987e6f15efa',
+            'reference' => 'a9f76b3807d45e4fd9ea64fc29ac27952aa37eb4',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
