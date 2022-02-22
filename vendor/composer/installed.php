685d0352d7da65bc0feccde184155fafb1675a7b
samiff <samiff@users.noreply.github.com>
2022-02-22 21:50:55 +0000

    Add Resumable Upload Support to the VideoPress Block (#22584)
    * UI stub with styles and VideoPress logo.
    
    * WIP Tus uploader for collaboration with teammate.
    
    * Adding use-uploader.js
    
    * Get upload jwt.
    
    * Bug fixes to get it kinda working!
    
    * Handle upload errors and upload completed events
    
    * Set up resumable block to be removable by using `useBlockProps`. Stop the Tus upload if the block is removed.
    
    * Added an error state and retry UI. Still needs testing :D
    
    * Added error state w/ try again and cancel button. Cleaned up some `console.log` debug lines.
    
    * Fixed issue where drag and drop would still use the VP block even if it was disabled.
    
    * Fixed error when refreshing the page while the video was uploading.
    
    * Grab the guid!
    
    * Fixed media library selection.
    
    * Added is_automattician() check for enabling resumable uploads.
    
    * Force-enable resumable uploads for now
    
    * Removed a few console logs.
    
    * Adding changelog file
    
    * Linting fixes
    
    * PHP linting
    
    * Updating package.json and pnpm-lock
    
    * Fixing variable name to fix resumable upload token append.
    
    * Fixes for handling completed upload.
    
    * Moving enqueue of js that enables resumable uploads so it works on wp.com
    
    * Set the background color of the resumable uploader to always be white.
    
    * Changing endpoint to a wpcom/v2 which works better on wpcom and Jetpack
    
    * Disable drop area for the resumable upload placeholder
    
    * Get a fresh JWT when doing HEAD requests to determine if an upload can
    be resumed.
    
    * PHP lint fix
    
    * Code cleanup based on feedback.
    
    * Update error string to differentiate better
    
    Co-authored-by: John Caruso <johncaruso@gmail.com>
    
    * Removing unused header value
    
    * Remove `resume` option that is now deprecated.
    
    * Use camelCase for a few svg args
    
    * Create a new props object to pass to the original placeholder component.
    
    Co-authored-by: Panos Kountanis <panosktn@gmail.com>
    Co-authored-by: John Caruso <johncaruso@gmail.com>
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1884100530

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '54907c9606906af17d3b7e5e93733b45fed227d6',
+            'reference' => 'a4fe94a394d3af1327739d6e6af54fd61a8a8231',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
