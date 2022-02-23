a5012fc083dc22c6490b64e49ad96045bb105017
oskosk <oskosk@users.noreply.github.com>
2022-02-23 18:22:23 +0000

    My Jetpack: Memoize RecordEvent from useAnalytics hook (#23068)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1888946051

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '218df6bf0e9cbdf7e0bbabcc3447abb86c927a6e',
+            'reference' => '0ec798f4461b53543434136e65f4e76256cda81c',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
