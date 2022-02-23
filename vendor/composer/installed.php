8009cdd4ca5a7531dc7ee4ceaff977772110a968
kangzj <kangzj@users.noreply.github.com>
2022-02-23 01:15:01 +0000

    Search package: add i18n support for auto added search block (#23049)
    * added i18n support for search button
    
    * removed block label
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1884845605

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '28bfe406cbbcca2fea6db7412d72e88af0a0debf',
+            'reference' => '497c024eec69cd962bee87978cb982de3a24b8ce',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
