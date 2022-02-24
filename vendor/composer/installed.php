5633c59a001f5359d9e0542ef4ebf31033ca59a2
sergeymitr <sergeymitr@users.noreply.github.com>
2022-02-24 15:25:05 +0000

    IDC: bring back the IDC screen in Staging mode (#23053)
    * bring back the proper IDC mode for the sites in staging mode
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1893776473

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '8a15033fc2a15ebabed57061f1642f92b65f140d',
+            'reference' => '0015e671e3feb8790a89d069f5fd33b9a68496a4',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
