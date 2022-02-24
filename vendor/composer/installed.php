524110a3209d04fa5334bea9c85cd2259a6668cb
retrofox <retrofox@users.noreply.github.com>
2022-02-24 20:40:43 +0000

    My Jetpack: set height of Jetpack logo in the footer (#23095)
    * [not verified] my-jetpack: set height of the Logo in the footer
    
    * [not verified] changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1895208495

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'b605621cc3544bbcc1260ee19c42e62b31219d72',
+            'reference' => 'd53b49a1fde0f39790fd1785dc8a1185ea8aed7b',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
