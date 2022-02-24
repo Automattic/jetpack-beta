b0bb647f283b48e175a038c2b05662da20537054
leogermani <leogermani@users.noreply.github.com>
2022-02-24 20:28:32 +0000

    add connected plugins to Disconnect dialog (#23097)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1895151975

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'a469217cd695af798ec157193063e51117a90df1',
+            'reference' => 'd15abd3e1dfcf99efd1f33b733cc2b96702fdaca',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
