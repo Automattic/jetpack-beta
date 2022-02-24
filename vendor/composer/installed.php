219d6a0d863b8cd69ad4c418a23b7be5c884dc74
zinigor <zinigor@users.noreply.github.com>
2022-02-24 21:55:42 +0000

    Hoist the Terms of Service package into Jetpack Connection. (#22977)
    * Moved the source file, added a stub in place of the old one.
    
    * Moved the test suite.
    
    * Removed terms-of-service composer dependencies.
    
    * Updated Jetpack's composer lock.
    
    * Added require_once for a case of several autoloaders on the site.
    
    * Added changelog entries for packages.
    
    * Added the changelogger dependency back.
    
    * Fixed up versions.
    
    * Added changelog entries for changed packages.
    
    * Update projects/packages/tracking/changelog/hoist-jetpack-terms-of-service
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Update projects/plugins/jetpack/changelog/hoist-jetpack-terms-of-service
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Removed test and coverage scripts, purged test folder.
    
    * Fixed up versions.
    
    * Fixed up project versions.
    
    * UPdated composer, fixed lock files.
    
    * Fixed versions.'
    
    * Updated package versions.
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1895512662

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '363fcd238f4ed65dad6bd1a7f699aeff5ef072b8',
+            'reference' => 'eb698500661da93e4bfb551c26a51c3053c68e83',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
