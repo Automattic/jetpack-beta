0ed53854af0298aa08d5129dc4b49d9c3ea80f91
sdixon194 <sdixon194@users.noreply.github.com>
2022-02-24 22:33:43 +0000

    Publicize: Fix post meta not saving properly in Classic Editor (#23101)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1895672645

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '86fa0f5530de9c9a6d904614a0db09920fdaa8b0',
+            'reference' => '2806d47826d1537cb8ca77d7063fda5b2705296a',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
