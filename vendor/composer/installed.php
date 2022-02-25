90c6ad988787196bb214820074370937630943d6
oskosk <oskosk@users.noreply.github.com>
2022-02-25 20:35:55 +0000

    My Jetpack: Declare usage of WP_Error in Products class to prevent fatal (#23124)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1900648075

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'b7101acb6a3e45b607f9a6097fa54afda77127ee',
+            'reference' => '128917e13c9c2af24989fcbe5d1dee27073a1339',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
