dcd13d0fb9e68fea8d14976ac0e6b19d7e553abf
sergeymitr <sergeymitr@users.noreply.github.com>
2022-02-25 19:56:31 +0000

    Connection: remove trailing semicolons from the site suffix (#23125)
    Remove trailing semicolons from the site suffix.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1900498850

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '33cacdacc33542c78b259e866e5b7fac9f626303',
+            'reference' => 'b7101acb6a3e45b607f9a6097fa54afda77127ee',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
