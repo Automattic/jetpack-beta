fcc236c5aa00c9ac781c2915e967a7a2b6513bcc
anomiex <anomiex@users.noreply.github.com>
2022-02-25 19:08:15 +0000

    pr-is-up-to-date: Speed up processing of tag pushes with paths (#23123)
    When processing a tag push and PATHS is set, we need to find the merge
    base to get the diff to determine if the paths are touched.
    
    But it's a lot faster to fetch 100 revisions at once than to fetch 10
    revisions 10 times. So instead of fetching to each PR's merge base,
    which will likely enough fetch a few revisions for every PR beyond a
    certain age, fetch arbitrary blocks of revisions.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1900302702

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c08084e352116ad79e76636cc20e972dc1cd7cdf',
+            'reference' => '33cacdacc33542c78b259e866e5b7fac9f626303',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
