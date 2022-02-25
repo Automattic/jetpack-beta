17fd6f98440f7e9f10d542a9e8ae9f7d0da07bb1
jeherve <jeherve@users.noreply.github.com>
2022-02-25 09:48:52 +0000

    Search: release new version to ship #23102 (#23104)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1897923506

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '2ba749826504d1afc525d25a1de9780e738f7159',
+            'reference' => 'f5a51672bd4cb1fecc85111269229ec6fd351760',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
