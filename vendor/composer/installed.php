b24cebcde44e88cd67f3039e55cd35692fcdb7d8
fgiannar <fgiannar@users.noreply.github.com>
2022-02-28 15:07:47 +0000

    Jetpack: Take pre_update_option filter into account when firing active module hooks (#23087)
    * Jetpack: Take pre_update_option filter into account when firing active module hooks
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1910917025

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'dbd706ed516cf926f4856e422cbd528631f69615',
+            'reference' => '1b75a65fc8651549ad67ebbe622ce35e42c14c73',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
