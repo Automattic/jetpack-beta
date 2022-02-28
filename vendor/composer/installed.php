9dec4b17ba43c2fd800f527f82f8796039366211
anomiex <anomiex@users.noreply.github.com>
2022-02-28 18:43:19 +0000

    actions: Fix branch detection in slack notification workflow (#23145)
    `github.ref` will always be master, because that's where `workflow_run`
    runs the workflow from. The branch with the failing workflow that
    triggered the `workflow_run` event is available as
    `github.event.workflow_run.head_branch`.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1911944041

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'e40d07ba6c2ba84dd0acd886f2e878f6edfea94f',
+            'reference' => '20b9a2638b0cc8fc256e52f62c3c41ebe744a049',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
