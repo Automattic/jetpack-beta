989289bf01c0e6ff67295c370cc9c8c662a61346
jeherve <jeherve@users.noreply.github.com>
2022-02-28 13:54:35 +0000

    Sync class.json-api-endpoints.php from wpcom (#23122)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1910568352

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'adf61da0f935846ab440a0f25e7bbbb3b1b79aae',
+            'reference' => 'dbd706ed516cf926f4856e422cbd528631f69615',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
