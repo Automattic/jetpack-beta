a3fc7665600b0bb510be0abe97fa406b47038a59
anomiex <anomiex@users.noreply.github.com>
2022-03-01 18:34:26 +0000

    Update dependency dealerdirect/phpcodesniffer-composer-installer to v0.7.2 (#23172)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1917967804

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c7849539520917f069d0b4ad75734d7bfc5e112a',
+            'reference' => 'baa3ebf1695edb172a6b9224610424c23cd91661',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
