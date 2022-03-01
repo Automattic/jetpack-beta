14a278119d1b92aa1b29137dd00b451c7f03229e
sdixon194 <sdixon194@users.noreply.github.com>
2022-03-01 16:03:25 +0000

    Backport 10.7 Changelog (#23167)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1917212127

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '51942d60c4c292dd5d2835d7c70b2deab9d343bb',
+            'reference' => '5266d802b1814284b126edf19c1fa32a18a34192',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
