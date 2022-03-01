f669dad98b9d2ff06e796a63c93238cec3b50980
dsas <dsas@users.noreply.github.com>
2022-03-01 14:46:12 +0000

    Payments blocks: Add more robust set of keywords (#23056)
    Add a more extensive set of keywords to some of the payments blocks, to
    make it easier for users to find what they are looking for.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1916771251

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '9b5caf72f96b5a36e8abf93083307ff083c481a2',
+            'reference' => '2a0d86fc15287179a58c9ad5b5dca1a759d4d93a',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
