378382c01b7f98ba166352b00e3e7e6e9c1491a5
obenland <obenland@users.noreply.github.com>
2022-03-04 18:07:31 +0000

    Masterbar: Make Desktop Switcher look the same between Calypso and wp-admin (#22826)
    * [not verified] Masterbar: Make Desktop Switcher look the same between Calypso and wp-admin
    
    * [not verified] Add text domains
    
    * [not verified] Move styles out of html attribute
    
    * [not verified] Add changelog
    
    * [not verified] Let's remove that test for now, it's not overly useful.
    
    * [not verified] Fix some phpcs notices
    
    * [not verified] Enqueue script in footer so it can extend 'common'
    
    * [not verified] Make color vars available in wp-admin for core color schemes
    
    * [not verified] Properly position the selector on small screens
    
    * Use slug rather than a translated name
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1935138108

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '7c8a9f860b651ac0bd800b6050fe86ded256d62f',
+            'reference' => '866b38c15439c7bb43ed7e34d422148dc04379f4',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
