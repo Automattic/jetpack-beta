0ac1e61b7e30ceb3d552e6f93585ca86af6b1e24
oskosk <oskosk@users.noreply.github.com>
2022-03-04 16:16:17 +0000

    My Jetpack: tidy Product card component (#23224)
    Co-authored-by: oskosk <oskosk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1934676888

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '39cf7bdfe8558145897f0d63955f1aa95a87a544',
+            'reference' => '7c8a9f860b651ac0bd800b6050fe86ded256d62f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
