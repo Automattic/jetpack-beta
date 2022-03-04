13c3d4f202cc215df894ec9bfcd4f90d895df44b
anomiex <anomiex@users.noreply.github.com>
2022-03-04 19:28:53 +0000

    Resolve pnpm unmet peer deps (#22965)
    "Unmet" peer deps are those where the dependency is on a different
    version than that installed.
    
    * Update Jetpack's dep on `@size-limit/preset-app` to match its dep on
      `size-limit`, and have renovate group these in the future.
    * Add a `.pnpmfile.cjs` to set overrides for peer deps.
      * Anything depending on react or react-dom 16 can use 17 too.
      * Looks like `eslint-config-wpcalypso` is going to be going away in
    	favor of `eslint-plugin-wpcalypso` 5.1.0. But they haven't released
    	that yet, so until then hack its outdated peer deps.
    * Move all the pnpm overrides into `.pnpmfile.cjs` too, since we can
      have comments there.
      * Override Storybook's dep on `@types/webpack` along with `webpack`.
      * Drop the override for yargs-parser 5.0.0, they released a 5.0.1 with
    	the fix.
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1935483358

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '866b38c15439c7bb43ed7e34d422148dc04379f4',
+            'reference' => '32a9e725dd6a355fd09c2b71a9d08eef0a1d3b9d',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
