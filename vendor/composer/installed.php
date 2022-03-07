69dfb142c79dff905cfab0d4e94f1b59a6d8d95d
samiff <samiff@users.noreply.github.com>
2022-03-07 18:25:15 +0000

    Simplify query preparation in jp_purge_transients (#18871)
    * Simplify query preparation in jp_purge_transients
    
    The original query here is prepared in an overly complex way. I think this can be simplified to something that is much more readable.
    
    * Avoid linting errors
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1947186490

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'eac52a6feba754a9c311149915014fa05bdedeaf',
+            'reference' => '0b3b65c230cbd74d6ea6d9d7e498872b82aea586',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
