0b66d0b6f4fd1da777ca9c7499c5efd98b53d5b4
rcrdortiz <rcrdortiz@users.noreply.github.com>
2022-03-08 10:00:21 +0000

    Payments Button Block: Add payments block support reference link (#23217)
    * The Payments Block now has a link to the support reference page on the block configuration panel
    
    * Added Jetpack changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1950686750

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '711afb280b6f4cd4b09c1f418d4c1a6c64e68cc0',
+            'reference' => 'e470928198246ec2fcd6f311e6266631b3b81f33',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
