1c73573d9241de0527eceec8cd7285fa011fc21d
kraftbj <kraftbj@users.noreply.github.com>
2022-03-08 23:35:59 +0000

    Autoloader: Revert the Upgrade Handler (#23312)
    * Revert "autoloader: Preload classes on plugin upgrade to avoid mid-upgrade fatals (#23039)"
    
    This reverts commit ae5a139e2b982d13b999cfbfbe23bfe2cf9e5fa8.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1954381727

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c72778df0517937027c63222bddba33ced7980ac',
+            'reference' => '2bf91dae09bf8027dbeb3787f807e9793b5936d6',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
