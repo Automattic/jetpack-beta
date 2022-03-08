4dfc5d08a134d1b70c1999fb98fcba5edd788161
samiff <samiff@users.noreply.github.com>
2022-03-08 23:46:28 +0000

    Init 10.8-a.6 cycle. (#23314)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1954416988

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '2bf91dae09bf8027dbeb3787f807e9793b5936d6',
+            'reference' => 'f52769608c0556d16ce2f3f2ae69f82cbc0f66ed',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
