2f53a1d0af4b7a690c73252452001a059d2041da
anomiex <anomiex@users.noreply.github.com>
2022-03-14 20:18:23 +0000

    Update dependency global-jsdom to v8.4.0 (#23430)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1982879155

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'bdd924cf47969555f1cd977419c097e9f44cea3a',
+            'reference' => '7b2db5e1b5239f40a69dac08370ab13be2a84610',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
