265d065b56813a12ef9e2dd3fde6ca22e9bf5a78
kraftbj <kraftbj@users.noreply.github.com>
2022-03-14 19:31:34 +0000

    Testing: Add simple repo plugin (#23387)
    * Initial commit of Always Use for testing purposes
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1982684695

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '0f224606ed2f3a057e8c14a7782c37780f7a733d',
+            'reference' => 'bdd924cf47969555f1cd977419c097e9f44cea3a',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
