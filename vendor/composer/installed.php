d2c5b97c89a68d7af89d277d3037c37991deabdf
jeherve <jeherve@users.noreply.github.com>
2022-03-15 17:16:20 +0000

    Dashboard: avoid React warnings; use className instead of class (#23450)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1988135661

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '0e3073dc64b399de3526cf956c31e6e8f4660fbc',
+            'reference' => '10028eb33f212d78ea951cd25845e9832b8ca3a8',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
