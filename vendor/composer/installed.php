31b2aeb16665debe1b1c52e498c522d512824e87
jeherve <jeherve@users.noreply.github.com>
2022-03-16 07:42:19 +0000

    Admin Menu: Optimize user blog counting (#23447)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1991343002

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '6f589f14b72b5b351d02f276067abced1426fb2a',
+            'reference' => 'd1fcda33d4a135890ad319fee57995ee3ae3caf9',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
