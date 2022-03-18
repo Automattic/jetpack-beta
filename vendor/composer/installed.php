8e4783274752947da83f8df38d5bcf155963ece0
renatoagds <renatoagds@users.noreply.github.com>
2022-03-18 13:01:41 +0000

    My Jetpack: Usage of `Text` and design tokens at `ProductCard` (#23311)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2004198522

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'f2f037a677d81898b320df1468090496b0c8fa5d',
+            'reference' => '1eecb8447253c2bcadb49b9cf024842efdf8d771',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
