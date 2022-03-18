a73703ba96730d89d3878d896647afd1a2a30fd8
jeherve <jeherve@users.noreply.github.com>
2022-03-18 13:53:05 +0000

    Payments Block: Default URL opens the corresponding form (#23487)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2004439833

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '40996ae54c624e4ed035e0f2fec15b652b5b0ad7',
+            'reference' => 'd9eb6004b028aeb9466a5218511ff01e621e2c52',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
