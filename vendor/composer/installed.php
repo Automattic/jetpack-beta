5eade824c862775687d7cc94895e149f9a57945a
adimoldovan <adimoldovan@users.noreply.github.com>
2022-03-22 18:00:57 +0000

    E2E tests: ignore getDefaultProps console warning (#23543)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2023908717

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '2badfeb7f7540cf2a5228a6c0bd99befd5defe18',
+            'reference' => 'fb28697a1b02ae54fb83ed068e96d75b724067d5',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
