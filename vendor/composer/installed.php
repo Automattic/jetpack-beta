9973f0a877d90b39f0ec5cb9a1b0a8fa5d8aad61
jeherve <jeherve@users.noreply.github.com>
2022-03-22 17:55:11 +0000

    Shortlinks: ensure the block editor shows for CPTs. (#23556)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2023884051

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '385fa7c9bb67811ab15a6fc7deadbe5f421d8b7f',
+            'reference' => 'd4915ea4135841716e164558d9e5e1908b3c0a90',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
