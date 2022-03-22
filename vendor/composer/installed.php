67c98c91d260fabed57d97795d75d3e8633f8a59
retrofox <retrofox@users.noreply.github.com>
2022-03-22 20:50:23 +0000

    My Jetpack: Remove client code that handles the deactivation process of a product (#23552)
    * my-jetpack: remove showDeactivate prop
    
    * my-jetpack: remove deactivation from state
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2024765791

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'b537f95e1e03b60e12f2eb7a4d61a8745b3ea5b6',
+            'reference' => '50fcc878ae86ee2a503554367494d10b7f312d07',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
