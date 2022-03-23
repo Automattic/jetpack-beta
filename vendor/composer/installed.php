8a5385a7f99b3684c79e6ac91b495a31225f5e32
jeherve <jeherve@users.noreply.github.com>
2022-03-23 11:04:43 +0000

    PHPCS: fix linting errors in API files, part 2 (#23557)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2027930312

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'dfefb84627707163e37a1c9618ba554e3f6dd696',
+            'reference' => '143713d981192f87f0c8ee60033d99c4d057c72b',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
