9077befa1fef69cbdf2280b89c52ef35ac85c974
sdixon194 <sdixon194@users.noreply.github.com>
2022-03-23 22:06:36 +0000

    PHPCS: Protect (#23509)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2030968618

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '83acfb03154ceb4d5159dafbe8e5c663e198365f',
+            'reference' => 'cd8c91b6723fb0635743da8a892503b07efade94',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
