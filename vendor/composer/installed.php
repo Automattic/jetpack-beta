7bb72080f886d7aa81ffedb92fda3c07373ffa4b
leogermani <leogermani@users.noreply.github.com>
2022-03-24 18:14:28 +0000

    My Jetpack: Use different URLs for manage and purchase links in plans section (#23155)
    * [not verified] # This is a combination of 2 commits.
    
    Use different URLs for manage and purchase links in My Jetpack plans section
    
    * Add changelog file
    
    * remove extra line
    
    * Update redirect slug to my-jetpack-my-plans-purchase
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2035832112

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '6515188230ed5e3a1bf7f569f0c0b14ffd7fa888',
+            'reference' => 'ef653249bfbba2cd4ed383c67de24298d770885e',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
