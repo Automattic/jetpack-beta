719907383c45ad8571f45fa8d1954ecc487476f4
samiff <samiff@users.noreply.github.com>
2022-03-24 22:14:00 +0000

    packages/search: release version 0.11.3 (#23611)
    * packages/search: release version 0.11.3
    
    * [not verified] Revert "packages/search: release version 0.11.3"
    
    This reverts commit 79f4a2077393fe4a9f55ce3dd139e54fbdc9d8ed.
    
    * packages/search: release version 0.11.3
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2036870708

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '0a37cec64c6f5675ee2304f1aa910313fd1da2fd',
+            'reference' => 'ee294f87ae5a896f61fb63e77301e0fcb26beb69',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
