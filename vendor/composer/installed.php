4abc61e9de6e0b58bd71f8aeffed6d7754629054
oskosk <oskosk@users.noreply.github.com>
2022-03-25 17:43:22 +0000

    Components: Move product icons from My Jetpack into components package (#23624)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2041353318

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '81e3cd84e3e406ea043220af0fd68be827cc90e7',
+            'reference' => '6513aaf67b2ae71f2aede7bb1aa4ce0c25e9ff83',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
