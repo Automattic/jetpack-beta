e3a64727e1eabba5ebbfa9c15486c88025311b90
jeherve <jeherve@users.noreply.github.com>
2022-03-25 11:36:36 +0000

    PHPCS: handle PHPCS errors in API files, part 3 (#23573)
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2039720754

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '60f7e5f1cd12239f476ac4c912534edcac70e131',
+            'reference' => 'c1aff6b3aa3ea5236002ca35f5d7e26f2273e949',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
