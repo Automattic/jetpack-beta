63ec750d4e183f3a08f265daaf4b446962ba1667
oskosk <oskosk@users.noreply.github.com>
2022-03-25 13:01:42 +0000

    Backup: Use real site title (name) when showing progresss text on admin page (#23621)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2040047979

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'f55ce53d596df21ebb6e5a3d268e85535b7f9dde',
+            'reference' => 'b2c0e002f6f6a55ff21a22e38f79eb79bd028247',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
