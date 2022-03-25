399032e81dc2ba64bd5c2e918ab4016a7a8d4708
Copons <Copons@users.noreply.github.com>
2022-03-25 11:16:00 +0000

    Payment Blocks: Use correct product type nomenclature in the shared management controls (#23566)
    Add a list of strings to be used by the `ProductManagementControls` component and the `jetpack/membership-products` store, based on a given product type.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2039630424

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'aebf5ebf898fcfbab676ac0ed779971ffb7ca94e',
+            'reference' => '60f7e5f1cd12239f476ac4c912534edcac70e131',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
