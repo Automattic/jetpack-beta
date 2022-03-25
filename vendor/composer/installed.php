c5ccb54c7b886f6537b920b9bf6a13ed2cf35f2d
annacmc <annacmc@users.noreply.github.com>
2022-03-25 15:56:08 +0000

    Search: add notice box component for Record Meter (#23228)
    Co-authored-by: Chris R <chris.rosser@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2040861641

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '7fe9c50b464e58223d0d9cefe76318aa9575ce01',
+            'reference' => 'b0219aa6f08a4703aaa9ab6746fdd04232daeafb',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
