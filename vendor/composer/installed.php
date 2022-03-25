506bed33cc1af927f7db171cdd8d312c2347c503
samiff <samiff@users.noreply.github.com>
2022-03-25 19:11:49 +0000

    Jetpack: init 10.8-a.12 (#23637)
    * Jetpack: init 10.8-a.12
    
    * [not verified] Revert "Jetpack: init 10.8-a.12"
    
    This reverts commit fcaaca62fb38e52110ab79281d61649159aa4bbf.
    
    * Stub 10.8-a.11 release
    
    * Bump to 10.8.0-a.12
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2041747724

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'e328dfc07bf606f98e50c1f4856ebbe53ce2d628',
+            'reference' => 'b25731f4e3e618260f167a2504c98c455aade1d1',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
