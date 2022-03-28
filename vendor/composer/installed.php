48d310f7b204f8bf958700c24e9e468308c4b021
renatoagds <renatoagds@users.noreply.github.com>
2022-03-28 12:25:16 +0000

    My Jetpack: Fix logos quality at `ConnectionScreen` (#23639)
    * My Jetpack: Improve logos quality at Connect Screen
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2052190957

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '021c8dff02617e2ffaddac7102b785e2c23da764',
+            'reference' => 'c2639bb8a1767dce9c89400e8c9bb9124c9d0357',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
