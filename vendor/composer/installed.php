4d36aedc3684145342944565a8b65ebbfabf3a16
Copons <Copons@users.noreply.github.com>
2022-03-28 10:42:06 +0000

    Payment Button Block: Use the shared ProductManagementControls (#23545)
    * We now use the Product Management Controls component instead of having ad-hoc implementations of product management for each payment block.
    * Cleaned up the code and refactored the block to a functional component.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2051702123

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'abb7a946c12f11f1a37b181fb885f00a65201518',
+            'reference' => '021c8dff02617e2ffaddac7102b785e2c23da764',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
