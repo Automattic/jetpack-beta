f13f9469d914a5ae5dc65b11ef9b042c0256a2a5
anomiex <anomiex@users.noreply.github.com>
2022-03-30 17:31:06 +0000

    actions: Don't subject non-PR Gardening runs to concurrency (#23710)
    We already did that for pushes, but we should also do it for issue
    opening/closing.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2066375880

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '9d7344b738d3d332dc40541d40b3a303e90b1f38',
+            'reference' => '1d772c77f0dc1479d0692a4201b71a79b1daa624',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
