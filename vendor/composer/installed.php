d6a1facfc951bbfb816c236bf9cea6132d684995
oskosk <oskosk@users.noreply.github.com>
2022-03-30 22:47:12 +0000

    Protect: Change connection screen (#23718)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2067777849

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'f291e60086eb4db663852aa02572035ee4969d44',
+            'reference' => '215ea2bd8938c99441f9b760ebd38987e598631d',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
