ad6a18274c528d1e0004e4254cf98d05557753f8
anomiex <anomiex@users.noreply.github.com>
2022-03-31 13:30:09 +0000

    changelogger: Deduplicate input entries (#23712)
    If we have a dozen change entries that all say "Updated package
    dependencies", those should be deduplicated.
    
    That should happen even if `--deduplicate=0`. Allow setting
    `--deduplicate=-1` to disable it if necessary.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2071275970

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '463d0e65a21d17d5ef784a0ead6805426370835f',
+            'reference' => '472b69e21b5023a5922ea9947b363ab40f2d6610',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
