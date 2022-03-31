e5ed0fe93e01beb57fe01f7b8cc3f5c35fdd6f1a
adimoldovan <adimoldovan@users.noreply.github.com>
2022-03-31 12:36:51 +0000

    E2E tests: remove duplicated test fixture (#23703)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2070998798

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '215ea2bd8938c99441f9b760ebd38987e598631d',
+            'reference' => '463d0e65a21d17d5ef784a0ead6805426370835f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
