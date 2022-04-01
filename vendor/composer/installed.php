d6198acb9b728aca1f7458d1588370a23970f006
anomiex <anomiex@users.noreply.github.com>
2022-04-01 20:01:04 +0000

    Fix pnpm-lock.yaml (#23762)
    For some reason #23617 downgraded `@types/node`, removing the entry for
    the newer version, while #23745 upgraded a few references to that same
    newer version.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2079423852

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'ed06efcc8b3fe436cfed0576aa5b5ad3b7784358',
+            'reference' => 'a76058ca3adf50b0978c2dc9c59478dc2326516a',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
