c35e710f45001ed9a5d9fb04de87c3ad8319fe7b
anomiex <anomiex@users.noreply.github.com>
2022-04-01 19:09:46 +0000

    Update dependency @mdn/browser-compat-data to v4.1.14 (#23747)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2079190409

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '12702c29fcb49bcc315082d27d6d0d46e0590cbc',
+            'reference' => 'ed06efcc8b3fe436cfed0576aa5b5ad3b7784358',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
