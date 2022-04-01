1e2e4c9411d815172b9dc9c94fd45c0c94a73928
sdixon194 <sdixon194@users.noreply.github.com>
2022-04-01 20:58:37 +0000

    PHPCS: JSON GET Endpoints Part 2 (#23610)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2079669558

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'a76058ca3adf50b0978c2dc9c59478dc2326516a',
+            'reference' => '409998d11fa5a45ed6bc35bcadb5cfa12ae3af81',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
