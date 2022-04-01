076f42d232e0deb7bf01d254f4a61bed946e0d35
anomiex <anomiex@users.noreply.github.com>
2022-04-01 16:58:32 +0000

    tools: Let check-changelogger-use.php detect uncommitted change entries (#23732)
    If change entry files exist but aren't committed, the script should say
    so instead of saying that they need to be added.
    
    Further, when run from the pre-push hook, it would be nice if it would
    offer to commit the entries.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2078595176

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c6a3f1f9668debb4bc6d454b12eb2df878d5c31e',
+            'reference' => '12702c29fcb49bcc315082d27d6d0d46e0590cbc',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
