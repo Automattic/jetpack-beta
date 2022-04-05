2e57b60638de87e3e7465fe2604e47240e0bc90f
anomiex <anomiex@users.noreply.github.com>
2022-04-05 20:26:42 +0000

    build: Avoid races when writing composer.json (#23802)
    Every once in a while the parallel build fails because one process is
    writing composer.json at the same moment another process tries to read
    it. The reader sees an empty or incomplete file, and composer chokes.
    
    We can avoid these races by writing the file to a temporary name then
    renaming it, as a file rename should be an atomic operation.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2098563315

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'aed257643093c888cb64e876000417566dd7ad25',
+            'reference' => 'e8ca5e334556450ac41bb6f3dd76b6fdb28f90dc',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
