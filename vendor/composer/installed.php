df870fb08454cc2348399065273f99ae3000754a
retrofox <retrofox@users.noreply.github.com>
2022-04-06 18:37:47 +0000

    JS Components: fix className prop in Button component (#23829)
    * js-components: fix propsClassName prop issue
    
    * js-component: add className prop to Primary story
    
    * changelog
    
    * simplify adding className
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2104341617

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '69513b84acfd82bbf0471a198d91ca48e56b1c20',
+            'reference' => '7016868685dc5bfc4d3ccb3a35ca2d9fee113718',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
