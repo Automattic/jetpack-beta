70cfc690c5ac81cf84ef9377bf42c38dd6b809c4
kraftbj <kraftbj@users.noreply.github.com>
2022-04-06 20:58:22 +0000

    Starter Plugin: Serve via Beta Plugin (#23836)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2105015537

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'b4fbecd18d53dc4f51f0d325f22c1872959f50d3',
+            'reference' => '96720d71e1250622495fd45e4da228443b54afce',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
