292252cbc97d3a20d0a9c2339fda7904985ee2fa
oskosk <oskosk@users.noreply.github.com>
2022-04-06 18:31:29 +0000

    Protect: Add list of installed pĺugins to the initial state (#23830)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2104311023

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'ae138a335af01ff768b5e342957e3cfc1c27769f',
+            'reference' => '69513b84acfd82bbf0471a198d91ca48e56b1c20',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
