4086d28ae408ec8b419319fd20cafb955be2ccf3
oskosk <oskosk@users.noreply.github.com>
2022-04-06 17:10:44 +0000

    RNA: Export `Button` and fix `Button` link variant padding and target (#23825)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2103898404

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '40addc35991deb0f9cd5c6b20bef01b9bef20283',
+            'reference' => 'c20d83cd31b4fbb49e79649b0a8faf67d965e1d1',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
