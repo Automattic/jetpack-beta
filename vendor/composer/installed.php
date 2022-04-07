74040ba7799a287ff5e9522d23d88d88a715cf00
anomiex <anomiex@users.noreply.github.com>
2022-04-07 14:52:29 +0000

    jetpack: Build sass files with webpack (#23840)
    This allows them to take care of the better optimization in the newer
    toolchain, and lets us work towards reducing dependencies by
    consolidating on a single frontend build tool.
    
    Other than dropping some unnecessary comments, the new toolchain seems
    to save between 11 and 557 bytes per minified file, for a total of
    5765 across all the files in this PR. Dropping the "this is compiled
    SASS code" comments saves another 5000-ish on top of that. Build
    time locally doesn't seem unduly affected.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2109639600

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '3ca081762f17462fa9d510cc3b8b052825370656',
+            'reference' => '080af89b5df6fde4d8215c00b127f7ef8fed3593',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
