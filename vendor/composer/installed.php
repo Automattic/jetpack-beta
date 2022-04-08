63594e4ad84cdbd990b8be1d90b956ba5268b3ca
anomiex <anomiex@users.noreply.github.com>
2022-04-08 10:58:41 +0000

    composer-plugin: Set plugin-modifies-install-path flag (#23870)
    This setting, added in composer 2.2.9, signals to Composer that the
    plugin will change the install paths of other packages and so should be
    installed early in the process.
    
    https://getcomposer.org/doc/articles/plugins.md#plugin-modifies-install-path
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2114691103

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '2083022faa79ae5d437f87e7b993ce9af455f7d4',
+            'reference' => '1e5371a1b9f57b18c578c02614001db5ff258d3d',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-beta' => array(
