4174a8dd3bea5173583201f169faf0431efc3745
kraftbj <kraftbj@users.noreply.github.com>
2022-04-28 13:39:01 +0000

    Fix PHPCS warning (#24141)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2239575142

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -6,21 +6,33 @@ $vendorDir = dirname(__DIR__);
 $baseDir   = dirname($vendorDir);
 
 return array(
+	'Composer\\Semver\\Interval' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
+	),
+	'Composer\\Semver\\Intervals' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
+	),
 	'Composer\\Semver\\Comparator' => array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
 	),
-	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
+	'Composer\\Semver\\CompilingMatcher' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
+	),
+	'Composer\\Semver\\VersionParser' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
+	),
+	'Composer\\Semver\\Semver' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
 	),
 	'Composer\\Semver\\Constraint\\MatchNoneConstraint' => array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchNoneConstraint.php'
-	),
-	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
 	),
 	'Composer\\Semver\\Constraint\\Constraint' => array(
 		'version' => '3.2.7.0',
@@ -34,57 +46,45 @@ return array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
 	),
-	'Composer\\Semver\\VersionParser' => array(
+	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
 	),
-	'Composer\\Semver\\Interval' => array(
+	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
-	),
-	'Composer\\Semver\\CompilingMatcher' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
-	),
-	'Composer\\Semver\\Intervals' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
-	),
-	'Composer\\Semver\\Semver' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
-	),
-	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadFileWriter.php'
-	),
-	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadProcessor' => array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
 	),
+	'Automattic\\Jetpack\\Autoloader\\AutoloadGenerator' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadGenerator.php'
+	),
 	'Automattic\\Jetpack\\Autoloader\\ManifestGenerator' => array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
 	),
-	'Automattic\\Jetpack\\Autoloader\\AutoloadGenerator' => array(
+	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
 		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadGenerator.php'
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
+	),
+	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadFileWriter.php'
 	),
 	'Parsedown' => array(
 		'version' => '1.7.4.0',
 		'path'    => $vendorDir . '/erusev/parsedown/Parsedown.php'
 	),
+	'Automattic\\JetpackBeta\\PluginDataException' => array(
+		'version' => 'dev-master',
+		'path'    => $baseDir . '/src/class-plugindataexception.php'
+	),
 	'Automattic\\JetpackBeta\\CliCommand' => array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-clicommand.php'
-	),
-	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
-		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-autoupdateself.php'
 	),
 	'Automattic\\JetpackBeta\\ParsedownExt' => array(
 		'version' => 'dev-master',
@@ -94,20 +94,20 @@ return array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-admin.php'
 	),
-	'Automattic\\JetpackBeta\\PluginDataException' => array(
+	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-plugindataexception.php'
+		'path'    => $baseDir . '/src/class-autoupdateself.php'
 	),
-	'Automattic\\JetpackBeta\\Hooks' => array(
+	'Automattic\\JetpackBeta\\Utils' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-hooks.php'
+		'path'    => $baseDir . '/src/class-utils.php'
 	),
 	'Automattic\\JetpackBeta\\Plugin' => array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-plugin.php'
 	),
-	'Automattic\\JetpackBeta\\Utils' => array(
+	'Automattic\\JetpackBeta\\Hooks' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-utils.php'
+		'path'    => $baseDir . '/src/class-hooks.php'
 	),
 );
