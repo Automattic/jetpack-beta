90c50cb5c1aeb1dca967547ea56c6466526729da
anomiex <anomiex@users.noreply.github.com>
2022-06-15 18:26:34 +0000

    Centralize jest configs (#24700)
    By creating and using a `tools/js-tools/jest/config.base.js`, we can get
    rid of a bunch of boilerplate in the jest configs. In particular, we no
    longer need to copy `jest-jetpack-config.js` and
    `jest-extensions-babel-transform.js` all over the place.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2504170476

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -6,13 +6,17 @@ $vendorDir = dirname(__DIR__);
 $baseDir   = dirname($vendorDir);
 
 return array(
-	'Composer\\Semver\\Constraint\\Bound' => array(
+	'Composer\\Semver\\Intervals' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/Bound.php'
+		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
 	),
-	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
+	'Composer\\Semver\\VersionParser' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
+		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
+	),
+	'Composer\\Semver\\Interval' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
 	),
 	'Composer\\Semver\\Constraint\\Constraint' => array(
 		'version' => '3.2.7.0',
@@ -22,13 +26,25 @@ return array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
 	),
+	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+	),
+	'Composer\\Semver\\Constraint\\Bound' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/Bound.php'
+	),
 	'Composer\\Semver\\Constraint\\MatchNoneConstraint' => array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchNoneConstraint.php'
 	),
-	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
+	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
+	),
+	'Composer\\Semver\\CompilingMatcher' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
 	),
 	'Composer\\Semver\\Semver' => array(
 		'version' => '3.2.7.0',
@@ -38,30 +54,6 @@ return array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
 	),
-	'Composer\\Semver\\VersionParser' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
-	),
-	'Composer\\Semver\\Intervals' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
-	),
-	'Composer\\Semver\\Interval' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
-	),
-	'Composer\\Semver\\CompilingMatcher' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
-	),
-	'Automattic\\Jetpack\\Autoloader\\AutoloadProcessor' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
-	),
-	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
-	),
 	'Automattic\\Jetpack\\Autoloader\\ManifestGenerator' => array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
@@ -69,6 +61,14 @@ return array(
 	'Automattic\\Jetpack\\Autoloader\\AutoloadGenerator' => array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadGenerator.php'
+	),
+	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
+	),
+	'Automattic\\Jetpack\\Autoloader\\AutoloadProcessor' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
 		'version' => '2.11.9999999.9999999-dev',
@@ -78,14 +78,6 @@ return array(
 		'version' => '1.7.4.0',
 		'path'    => $vendorDir . '/erusev/parsedown/Parsedown.php'
 	),
-	'Automattic\\JetpackBeta\\ParsedownExt' => array(
-		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-parsedownext.php'
-	),
-	'Automattic\\JetpackBeta\\PluginDataException' => array(
-		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-plugindataexception.php'
-	),
 	'Automattic\\JetpackBeta\\Admin' => array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-admin.php'
@@ -94,20 +86,28 @@ return array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-plugin.php'
 	),
-	'Automattic\\JetpackBeta\\Hooks' => array(
+	'Automattic\\JetpackBeta\\CliCommand' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-hooks.php'
-	),
-	'Automattic\\JetpackBeta\\Utils' => array(
-		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-utils.php'
+		'path'    => $baseDir . '/src/class-clicommand.php'
 	),
 	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
 		'version' => 'dev-master',
 		'path'    => $baseDir . '/src/class-autoupdateself.php'
 	),
-	'Automattic\\JetpackBeta\\CliCommand' => array(
+	'Automattic\\JetpackBeta\\Utils' => array(
 		'version' => 'dev-master',
-		'path'    => $baseDir . '/src/class-clicommand.php'
+		'path'    => $baseDir . '/src/class-utils.php'
+	),
+	'Automattic\\JetpackBeta\\Hooks' => array(
+		'version' => 'dev-master',
+		'path'    => $baseDir . '/src/class-hooks.php'
+	),
+	'Automattic\\JetpackBeta\\ParsedownExt' => array(
+		'version' => 'dev-master',
+		'path'    => $baseDir . '/src/class-parsedownext.php'
+	),
+	'Automattic\\JetpackBeta\\PluginDataException' => array(
+		'version' => 'dev-master',
+		'path'    => $baseDir . '/src/class-plugindataexception.php'
 	),
 );
