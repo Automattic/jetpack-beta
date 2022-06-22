39f68325058470cf9a5d5d0d3aea2cb7af9b4807
dsas <dsas@users.noreply.github.com>
2022-06-22 09:09:13 +0000

    Remove recurring-payments button color (#24801)
    The Payments Button button color was hardcoded to white (#fff). The
    button itself's color could be overriden by using the editor controls
    but the outline style just meant the button was invisible - white on
    white backgrounds.
    
    This has been fixed by removing the rule completely - I can't see why
    it exists. Another approach would have been to use the currentColor
    pseudo-color.
    
    Fixes #24768
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2541086644

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -6,33 +6,21 @@ $vendorDir = dirname(__DIR__);
 $baseDir   = dirname($vendorDir);
 
 return array(
-	'Composer\\Semver\\Intervals' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
-	),
-	'Composer\\Semver\\VersionParser' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
-	),
 	'Composer\\Semver\\Interval' => array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
 	),
-	'Composer\\Semver\\Constraint\\Constraint' => array(
+	'Composer\\Semver\\Comparator' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/Constraint.php'
-	),
-	'Composer\\Semver\\Constraint\\MatchAllConstraint' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
-	),
-	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
 	),
 	'Composer\\Semver\\Constraint\\Bound' => array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/Bound.php'
+	),
+	'Composer\\Semver\\Constraint\\MatchAllConstraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
 	),
 	'Composer\\Semver\\Constraint\\MatchNoneConstraint' => array(
 		'version' => '3.2.7.0',
@@ -42,6 +30,14 @@ return array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
 	),
+	'Composer\\Semver\\Constraint\\Constraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/Constraint.php'
+	),
+	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+	),
 	'Composer\\Semver\\CompilingMatcher' => array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
@@ -50,9 +46,13 @@ return array(
 		'version' => '3.2.7.0',
 		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
 	),
-	'Composer\\Semver\\Comparator' => array(
+	'Composer\\Semver\\VersionParser' => array(
 		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
+		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
+	),
+	'Composer\\Semver\\Intervals' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\ManifestGenerator' => array(
 		'version' => '2.11.9999999.9999999-dev',
@@ -62,10 +62,6 @@ return array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadGenerator.php'
 	),
-	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
-	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadProcessor' => array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
@@ -74,9 +70,33 @@ return array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadFileWriter.php'
 	),
+	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
+	),
 	'Parsedown' => array(
 		'version' => '1.7.4.0',
 		'path'    => $vendorDir . '/erusev/parsedown/Parsedown.php'
+	),
+	'Automattic\\JetpackBeta\\PluginDataException' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-plugindataexception.php'
+	),
+	'Automattic\\JetpackBeta\\Hooks' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-hooks.php'
+	),
+	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-autoupdateself.php'
+	),
+	'Automattic\\JetpackBeta\\CliCommand' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-clicommand.php'
+	),
+	'Automattic\\JetpackBeta\\ParsedownExt' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-parsedownext.php'
 	),
 	'Automattic\\JetpackBeta\\Admin' => array(
 		'version' => 'dev-trunk',
@@ -86,28 +106,8 @@ return array(
 		'version' => 'dev-trunk',
 		'path'    => $baseDir . '/src/class-plugin.php'
 	),
-	'Automattic\\JetpackBeta\\CliCommand' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-clicommand.php'
-	),
-	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-autoupdateself.php'
-	),
 	'Automattic\\JetpackBeta\\Utils' => array(
 		'version' => 'dev-trunk',
 		'path'    => $baseDir . '/src/class-utils.php'
-	),
-	'Automattic\\JetpackBeta\\Hooks' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-hooks.php'
-	),
-	'Automattic\\JetpackBeta\\ParsedownExt' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-parsedownext.php'
-	),
-	'Automattic\\JetpackBeta\\PluginDataException' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-plugindataexception.php'
 	),
 );
