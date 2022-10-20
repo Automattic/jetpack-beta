22467a58b5aa68b69e213cc705a1171ffccf5395
anomiex <anomiex@users.noreply.github.com>
2022-10-20 14:39:28 +0000

    autoloader, composer-plugin: Sort data in generated files (#26929)
    We don't get the packages to process in a consistent order, and the
    order of the data in the associative arrays in composer-plugin's
    `i18n-map.php` and the autoloader's `jetpack_autoloader_classmap.php`
    doesn't matter. So let's sort those arrays so diffs are cleaner.
    
    Note the data for the autoloader files (`jetpack_autoloader_filemap.php`
    and `jetpack_autoloader_psr4.php`) is already sorted when it's
    generated, so we don't have to do anything for those.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3290384280

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -6,61 +6,37 @@ $vendorDir = dirname(__DIR__);
 $baseDir   = dirname($vendorDir);
 
 return array(
-	'Composer\\Semver\\Interval' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
+	'Automattic\\JetpackBeta\\Admin' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-admin.php'
 	),
-	'Composer\\Semver\\Constraint\\Bound' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/Bound.php'
+	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-autoupdateself.php'
 	),
-	'Composer\\Semver\\Constraint\\Constraint' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/Constraint.php'
+	'Automattic\\JetpackBeta\\CliCommand' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-clicommand.php'
 	),
-	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+	'Automattic\\JetpackBeta\\Hooks' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-hooks.php'
 	),
-	'Composer\\Semver\\Constraint\\MatchNoneConstraint' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchNoneConstraint.php'
+	'Automattic\\JetpackBeta\\ParsedownExt' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-parsedownext.php'
 	),
-	'Composer\\Semver\\Constraint\\MatchAllConstraint' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
+	'Automattic\\JetpackBeta\\Plugin' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-plugin.php'
 	),
-	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
+	'Automattic\\JetpackBeta\\PluginDataException' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-plugindataexception.php'
 	),
-	'Composer\\Semver\\Comparator' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
-	),
-	'Composer\\Semver\\CompilingMatcher' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
-	),
-	'Composer\\Semver\\Semver' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
-	),
-	'Composer\\Semver\\Intervals' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
-	),
-	'Composer\\Semver\\VersionParser' => array(
-		'version' => '3.2.7.0',
-		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
-	),
-	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
-	),
-	'Automattic\\Jetpack\\Autoloader\\ManifestGenerator' => array(
-		'version' => '2.11.9999999.9999999-dev',
-		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
+	'Automattic\\JetpackBeta\\Utils' => array(
+		'version' => 'dev-trunk',
+		'path'    => $baseDir . '/src/class-utils.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
 		'version' => '2.11.9999999.9999999-dev',
@@ -74,40 +50,64 @@ return array(
 		'version' => '2.11.9999999.9999999-dev',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/AutoloadProcessor.php'
 	),
+	'Automattic\\Jetpack\\Autoloader\\CustomAutoloaderPlugin' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/CustomAutoloaderPlugin.php'
+	),
+	'Automattic\\Jetpack\\Autoloader\\ManifestGenerator' => array(
+		'version' => '2.11.9999999.9999999-dev',
+		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
+	),
+	'Composer\\Semver\\Comparator' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Comparator.php'
+	),
+	'Composer\\Semver\\CompilingMatcher' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/CompilingMatcher.php'
+	),
+	'Composer\\Semver\\Constraint\\Bound' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/Bound.php'
+	),
+	'Composer\\Semver\\Constraint\\Constraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/Constraint.php'
+	),
+	'Composer\\Semver\\Constraint\\ConstraintInterface' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/ConstraintInterface.php'
+	),
+	'Composer\\Semver\\Constraint\\MatchAllConstraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchAllConstraint.php'
+	),
+	'Composer\\Semver\\Constraint\\MatchNoneConstraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MatchNoneConstraint.php'
+	),
+	'Composer\\Semver\\Constraint\\MultiConstraint' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Constraint/MultiConstraint.php'
+	),
+	'Composer\\Semver\\Interval' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Interval.php'
+	),
+	'Composer\\Semver\\Intervals' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Intervals.php'
+	),
+	'Composer\\Semver\\Semver' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/Semver.php'
+	),
+	'Composer\\Semver\\VersionParser' => array(
+		'version' => '3.2.7.0',
+		'path'    => $vendorDir . '/composer/semver/src/VersionParser.php'
+	),
 	'Parsedown' => array(
 		'version' => '1.7.4.0',
 		'path'    => $vendorDir . '/erusev/parsedown/Parsedown.php'
-	),
-	'Automattic\\JetpackBeta\\Plugin' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-plugin.php'
-	),
-	'Automattic\\JetpackBeta\\PluginDataException' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-plugindataexception.php'
-	),
-	'Automattic\\JetpackBeta\\ParsedownExt' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-parsedownext.php'
-	),
-	'Automattic\\JetpackBeta\\CliCommand' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-clicommand.php'
-	),
-	'Automattic\\JetpackBeta\\Hooks' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-hooks.php'
-	),
-	'Automattic\\JetpackBeta\\AutoupdateSelf' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-autoupdateself.php'
-	),
-	'Automattic\\JetpackBeta\\Admin' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-admin.php'
-	),
-	'Automattic\\JetpackBeta\\Utils' => array(
-		'version' => 'dev-trunk',
-		'path'    => $baseDir . '/src/class-utils.php'
 	),
 );
