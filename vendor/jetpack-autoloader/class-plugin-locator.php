229a622fd7321687ba567fc2ea7430d9358ec036
anomiex <anomiex@users.noreply.github.com>
2023-11-21 09:47:30 +0000

    codesniffer: Enable and fix `Modernize.FunctionCalls.Dirname.Nested` sniff (#34218)
    The WordPress-Extra ruleset turns this off. Now that we've dropped PHP
    5.6, we can turn it on.
    
    Then, of course, we have to fix all the existing instances of the
    problem.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6941880745

--- vendor/jetpack-autoloader/class-plugin-locator.php
+++ vendor/jetpack-autoloader/class-plugin-locator.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_0_0_alpha\al3_0_0;
+namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_0_0_alpha\al3_0_1_alpha;
 
  // phpcs:ignore
 
@@ -39,7 +39,7 @@ class Plugin_Locator {
 	 */
 	public function find_current_plugin() {
 		// Escape from `vendor/__DIR__` to root plugin directory.
-		$plugin_directory = dirname( dirname( __DIR__ ) );
+		$plugin_directory = dirname( __DIR__, 2 );
 
 		// Use the path processor to ensure that this is an autoloader we're referencing.
 		$path = $this->path_processor->find_directory_with_autoloader( $plugin_directory, array() );
