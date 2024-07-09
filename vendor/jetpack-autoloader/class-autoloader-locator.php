e5f11125fc958e1ee6d2f48c9afc7ed279d43e87
anomiex <anomiex@users.noreply.github.com>
2024-07-09 12:12:55 +0000

    autoloader: Avoid a deprecation notice in `Autoloader_Locator::find_latest_autoloader()` (#38245)
    If one of the cached plugin paths has gone missing,
    `get_autoloader_version()` will return `null` when trying to check it.
    Avoid passing that `null` to `is_version_update_required()`.
    
    This sort of thing happens weekly during Atomic upgrades, because there
    they have symlinks `wp-content/plugins/jetpack` →
    `/wordpress/plugins/jetpack/latest` → `/wordpress/plugins/jetpack/<ver>`
    and the update process deletes the old `<ver>` at the same time as
    creating the new and updating the `latest` symlink.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9856596744
    
    Upstream-Ref: Automattic/jetpack@1893ee2996ed42a433a03b5a84310934bcf0bb9a

--- vendor/jetpack-autoloader/class-autoloader-locator.php
+++ vendor/jetpack-autoloader/class-autoloader-locator.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_1_1_alpha\al3_0_8;
+namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_1_1_alpha\al3_0_9_alpha;
 
  // phpcs:ignore
 
@@ -45,7 +45,7 @@ class Autoloader_Locator {
 
 		foreach ( $plugin_paths as $plugin_path ) {
 			$version = $this->get_autoloader_version( $plugin_path );
-			if ( ! $this->version_selector->is_version_update_required( $latest_version, $version ) ) {
+			if ( ! $version || ! $this->version_selector->is_version_update_required( $latest_version, $version ) ) {
 				continue;
 			}
 
