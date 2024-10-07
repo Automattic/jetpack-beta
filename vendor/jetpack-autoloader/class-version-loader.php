e71fa7fccc1e6ba5ed890c6a919a5fc6b0e72582
mreishus <mreishus@users.noreply.github.com>
2024-10-07 20:11:42 +0000

    find_psr4_file: Quick exit if psr4_map is not populated (#39646)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11222993561
    
    Upstream-Ref: Automattic/jetpack@c647351719be15f176431cf989428f2d19841206

--- vendor/jetpack-autoloader/class-version-loader.php
+++ vendor/jetpack-autoloader/class-version-loader.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_1_1_alpha\al3_1_0;
+namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_1_1_alpha\al3_1_1_alpha;
 
  // phpcs:ignore
 
@@ -143,7 +143,7 @@ class Version_Loader {
 	 * @return array|null $data The version and path path to the file if found, null otherwise.
 	 */
 	private function find_psr4_file( $class_name ) {
-		if ( ! isset( $this->psr4_map ) ) {
+		if ( empty( $this->psr4_map ) ) {
 			return null;
 		}
 
