b0843dea240998eef847fc8d5518341b0d3fe3f3
anomiex <anomiex@users.noreply.github.com>
2025-04-22 20:22:39 +0000

    phpunit: Enable reporting and failing on PHPUnit 11 deprecations (#43179)
    Now that we've fixed all the PHPUnit 11 deprecations, adjust the configs
    to enable `displayDetailsOnPhpunitDeprecations` and
    `failOnPhpunitDeprecation`.
    
    For plugins/crm, plugins/jetpack, and plugins/wpcomsh, there's one more
    deprecation left that's also fixed in this PR: WP Core's obsolete
    `WP_UnitTestCase_Base::checkRequirements()` function has a doc comment
    that PHPUnit is mis-parsing to see a `@group` annotation that's not
    actually intended. We can override it to stop PHPUnit from seeing that
    doc comment.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14603629889
    
    Upstream-Ref: Automattic/jetpack@c78f78dae7d54c7cd98b5b189bd4c4485fb100c5

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -51,7 +51,7 @@ return array(
 		'path'    => $baseDir . '/src/class-utils.php'
 	),
 	'Automattic\\Jetpack\\Admin_UI\\Admin_Menu' => array(
-		'version' => '0.5.8.0-alpha1745351100',
+		'version' => '0.5.8.0-alpha1745352378',
 		'path'    => $vendorDir . '/automattic/jetpack-admin-ui/src/class-admin-menu.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
