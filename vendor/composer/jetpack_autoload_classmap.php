48ec2ed9141ed5f4c6b9fb1f28577bc6f33d3b86
anomiex <anomiex@users.noreply.github.com>
2025-04-24 16:57:38 +0000

    phpunit: Update to yoast/phpunit-polyfills v4 (#43231)
    This brings in use of PHPUnit 12 for PHP 8.3+. Other than needing
    `phpunit.12.xml.dist` files (as symlinks to the PHPUnit 11 configs),
    this seems to work without issue after a few cleanups in #43215
    and #43217.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14646946984
    
    Upstream-Ref: Automattic/jetpack@320d92e7a0e6be41533b5f88f6b2fd0cdc13f163

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -51,7 +51,7 @@ return array(
 		'path'    => $baseDir . '/src/class-utils.php'
 	),
 	'Automattic\\Jetpack\\Admin_UI\\Admin_Menu' => array(
-		'version' => '0.5.8.0-alpha1745352378',
+		'version' => '0.5.8.0-alpha1745512883',
 		'path'    => $vendorDir . '/automattic/jetpack-admin-ui/src/class-admin-menu.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
