ce702a7d4f3391688413154f7de54d0d75f1a97a
kraftbj <kraftbj@users.noreply.github.com>
2025-07-24 20:46:47 +0000

    Autoloader: remove unneeded dev files from production package (#44456)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16507227019
    
    Upstream-Ref: Automattic/jetpack@136090b339b28c53c92264802b069d029acbb4ba

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_1_5_alpha\al5_0_8;
+namespace Automattic\Jetpack\Autoloader\jp567fa3f555de8fd218dfdc1688bb97b5_betaⓥ4_1_5_alpha\al5_0_9_alpha;
 
  // phpcs:ignore
 
