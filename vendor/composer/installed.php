bf63e5c674f073d4580f2f50ca97db1ea6fa6954
anomiex <anomiex@users.noreply.github.com>
2025-12-02 14:46:42 +0000

    Backport jetpack 15.3-beta changes (#46157)
    * Changelog and readme.txt edits.
    * Amend readme.txt
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Grzegorz Chudzinski-Pawlowski <>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19862089357
    
    Upstream-Ref: Automattic/jetpack@fe9328a088999d9b35a38975723313959f7cb417

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-admin-ui' => array(
             'pretty_version' => '0.5.11',
             'version' => '0.5.11.0',
-            'reference' => '8ef7acf40790b7a96819cf8ba3b0c8b5806d7043',
+            'reference' => '8dc499973966d77ce32f96d6dd32e04635b608d9',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-admin-ui',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '5.0.13',
             'version' => '5.0.13.0',
-            'reference' => 'd0a874a61e45206bf6f335809ecae00c53ab348f',
+            'reference' => 'c338e4bedacd364dee043b4ce78684e5dae03cef',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
