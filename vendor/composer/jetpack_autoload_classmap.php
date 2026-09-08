45f24ba5a0d9d23ac7bc74ee5a40c2dd7ebb151b
kraftbj <kraftbj@users.noreply.github.com>
2026-09-08 18:36:11 +0000

    Status: recognize the whole 127.0.0.0/8 loopback range and 0.0.0.0 as local (#51311)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34261847743
    
    Upstream-Ref: Automattic/jetpack@c636aaedd0796249314e68081e1165fca0b1cf29

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -83,15 +83,15 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-constants/src/class-constants.php'
 	),
 	'Automattic\\Jetpack\\CookieState' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-cookiestate.php'
 	),
 	'Automattic\\Jetpack\\Errors' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-errors.php'
 	),
 	'Automattic\\Jetpack\\Files' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-files.php'
 	),
 	'Automattic\\Jetpack\\IP\\Utils' => array(
@@ -99,11 +99,11 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-ip/src/class-utils.php'
 	),
 	'Automattic\\Jetpack\\Modules' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-modules.php'
 	),
 	'Automattic\\Jetpack\\Paths' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-paths.php'
 	),
 	'Automattic\\Jetpack\\Redirect' => array(
@@ -111,23 +111,23 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-redirect/src/class-redirect.php'
 	),
 	'Automattic\\Jetpack\\Status' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-status.php'
 	),
 	'Automattic\\Jetpack\\Status\\Cache' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-cache.php'
 	),
 	'Automattic\\Jetpack\\Status\\Host' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-host.php'
 	),
 	'Automattic\\Jetpack\\Status\\Request' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-request.php'
 	),
 	'Automattic\\Jetpack\\Status\\Visitor' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $vendorDir . '/automattic/jetpack-status/src/class-visitor.php'
 	),
 	'Composer\\Semver\\Comparator' => array(
