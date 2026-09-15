46168b4f6420b1299d8dc307b4229081e9029e7b
kraftbj <kraftbj@users.noreply.github.com>
2026-09-15 14:52:10 +0000

    Admin Menu: name the visibility states in the filter docblock (#52299)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34982285894
    
    Upstream-Ref: Automattic/jetpack@cd4b02b3b78539a2403fd4d25a3d2442969d6861

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -51,7 +51,7 @@ return array(
 		'path'    => $baseDir . '/src/class-utils.php'
 	),
 	'Automattic\\Jetpack\\Admin_UI\\Admin_Menu' => array(
-		'version' => '0.12.0.0-alpha1789416420',
+		'version' => '0.12.0.0-alpha1789482718',
 		'path'    => $vendorDir . '/automattic/jetpack-admin-ui/src/class-admin-menu.php'
 	),
 	'Automattic\\Jetpack\\Assets\\Logo' => array(
